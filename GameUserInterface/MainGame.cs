4eeffc3fc3649bd5245eb6df53be48e0223be2b6
roman2702rb <roman2702rb@gmail.com>
2020-07-11 12:52:23 +0300

    q

--- GameUserInterface/MainGame.cs
+++ GameUserInterface/MainGame.cs
@@ -153,7 +153,6 @@ namespace GameUserInterface
                  }
                  else
                  {
-
                      System.Threading.Thread.Sleep(2000); 
                      SwitchPlayers();
                      CloseCards();
